4227a61e3d15d695f0ac04a8fc78936d336bd282
Bianca Curutan <bmcurutan@gmail.com>
2016-11-06 20:49:22 -0800

    getting closer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project 4 - Tweeter Redux
 
-Time spent: 14 hours spent in total
+Time spent: 20 hours spent in total
 
 ## User Stories
 
@@ -13,17 +13,18 @@ The following **required** functionality is completed:
 - [x] Profile page
    - [x] Contains the user header view
    - [x] Contains a section with the users basic stats: # tweets, # following, # followers
+   - [x] Shows the users timeline
 - [x] Home Timeline
    - [x] Tapping on a user image should bring up that user's profile page
 
 The following **optional** features are implemented:
 
-- [ ] Profile Page
+- [x] Profile Page
    - [x] Implement the paging view for the user description.
    - [x] As the paging view moves, increase the opacity of the background screen. See the actual Twitter app for this effect
    - [x] Pulling down the profile page should blur and resize the header image.
-- [ ] Account switching
-   - [ ] Long press on tab bar to bring up Account view with animation
+- [x] Account switching
+   - [x] Long press on tab bar to bring up Account view with animation
    - [ ] Tap account to switch to
    - [ ] Include a plus button to Add an Account
    - [ ] Swipe to delete an account
--- Tweeter/AccountsViewController.swift
+++ Tweeter/AccountsViewController.swift
@@ -10,15 +10,17 @@ import UIKit
 
 class AccountsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var accounts: [User]! = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        accounts = [User.currentUser!, User.currentUser!]
     }
     
     // MARK: - IBAction
@@ -26,14 +28,21 @@ class AccountsViewController: UIViewController {
     @IBAction func onCancel(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+}
+
+extension AccountsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconMenuCell", for: indexPath) as! IconMenuCell
+        cell.titleLabel.text = accounts[indexPath.row].name
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return accounts.count
+    }
+}
 
+extension AccountsViewController: UITableViewDelegate {
+    
 }
